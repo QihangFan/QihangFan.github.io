52b267023a6ef4f2d6f1e10e250cda1a0af879e1
QihangFan <55906354+QihangFan@users.noreply.github.com>
2022-08-14 17:29:06 -0700

    Credit Modification

--- JavaScript/overlayInfoCredit.js
+++ JavaScript/overlayInfoCredit.js
@@ -5,9 +5,22 @@ creditInfo.innerHTML = `
     <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:300,400&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../../css/style.css">
     
-      <p class="p-HoverInfo" style="margin-bottom:10px;">
+<!--      <p class="p-HoverInfo" style="margin-bottom:10px;">-->
+<!--        Copyright © 2020 Qihang Fan. All Rights Reserved-->
+<!--      </p>-->
+      
+    <h2 class="h2-Dec" style="margin-top:40px; margin-bottom: 0;">
+        Credit
+    </h2>
+    <p class="p-HoverInfo">
+        Project Designer: Qihang Fan, Kangxin Wu<br>
+        Project Instructor: Peter Testa, Nero He<br>
+        Web Developer: Qihang Fan<br>
+        Web Development Instructor: Anthony Tran
+    </p>
+    <p class="p-HoverInfo" style="margin-bottom:10px;">
         Copyright © 2020 Qihang Fan. All Rights Reserved
-      </p>
+    </p>
 `
 
 class overlayCredit extends HTMLElement {
